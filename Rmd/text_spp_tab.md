982181f3dee9d70b7184ea4326b3189382d690d2
crosbya1 <crosby@ualberta.ca>
2026-03-16 12:19:56 -0600

    initial commit

---
+++ Rmd/text_spp_tab.md
@@ -0,0 +1,17 @@
+<left>
+
+# Species Information: Black-throated Green Warbler
+
+
+![](../www/btnw.jpg)
+
+<br>
+
+## Overview
+
+The black-throated green warbler (BTNW) is an uncommon forest songbird species in the OSR. 
+
+<br> 
+
+blah, blah, blah 
+
---
+++ ui.R
@@ -0,0 +1,67 @@
+
+tagList(
+  navbarPage(
+    theme = bslib::bs_theme(version = 5, bootswatch = "lux"),
+    id = 'tabs',
+    collapsible = TRUE,
+    
+    # Describes the top header bar with tabs
+    header = tagList(
+      tags$head(tags$link(href = "css/style_blank.css", rel = "stylesheet")
+      )
+    ),
+    title = HTML('<div style="margin-top: 0px;"><a href="https://github.com/alberta-conservation" target="_blank"><img src="abc-program-logo.png" height="80"></a></div>'),
+    windowTitle = "OSR Biodiversity Assessment tool",
+    tabPanel("Welcome", value = 'intro'),
+    tabPanel("Species description", value = 'spp'),
+    tabPanel("Vulnerability assessment", value = 'vulnerability'),
+    tabPanel("Risk Assessment", value = 'risk'),
+    tabPanel("Recommendations", value = 'recommendations')
+  ), 
+  tags$div(
+    useShinyjs(),
+    
+    # Intro tab
+    conditionalPanel(
+      condition="input.tabs == 'intro'",
+      div(style = "background-color: white; width: 100vw; margin: 0; padding: 0; display: flex; justify-content: center;",
+          tags$img(src = "osr.png", height = "300px",  style = "display: block; object-fit: contain; width: 100%; max-width: none;")),
+      
+      
+      fluidRow(
+        column(3, layout_sidebar(
+          sidebar = sidebar(
+            position = "left",
+            selectInput(
+              "spp",
+              "Select a species:",
+              choices = c("Black-throated green warbler")
+            )
+          )
+        )),
+        
+        column(9, div(id = "markdown-content", includeMarkdown("Rmd/text_intro_tab.md")))
+      )
+    )
+  ),
+  
+  conditionalPanel(
+    condition = "input.tabs == 'spp' || input.tabs == 'vulnerability'",
+    fluidRow(
+      column(12, 
+             conditionalPanel(
+               condition = "input.tabs == 'spp'",
+               div(id = "markdown-content", includeMarkdown("Rmd/text_spp_tab.md")))
+      ), 
+      column(12, 
+             conditionalPanel(
+               condition = "input.tabs == 'vulnerability'",
+               div(id = "markdown-content", includeMarkdown("Rmd/text_vulnerability_tab.md")))
+      )
+    )
+  )
+)
+
+
+
+
