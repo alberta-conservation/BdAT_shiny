50615e3a4a6f010d0d80cd0def1ec4aba9efe388
crosbya1 <crosby@ualberta.ca>
2026-03-17 12:09:26 -0600

    global

--- global.R
+++ global.R
@@ -3,11 +3,7 @@
 options(repos = c(CRAN = "https://cran.rstudio.com"))
 
 # List of required packages
-required_packages <- c(
-  "leaflet", "shiny", "rmarkdown", "markdown", "shinydashboard", "shinyjs", "shinyBS", "lubridate",
-  "shinycssloaders", "tidyverse", "bslib", "leafem", "glue", "purrr", "opticut", "readr", "utils",
-  "terra", "stringr", "DT", "httr", "RColorBrewer", "sf", "tools", "viridis", "knitr", "ggplot2", "stars"
-)
+required_packages <- c("shiny","shinyjs", "rmarkdown", "markdown", "bslib")
 
 # Load packages quietly
 invisible(lapply(required_packages, library, character.only = TRUE))
